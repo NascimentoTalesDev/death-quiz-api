a040ec4f3d78a14056bfebec0870c3af4aaa65bd
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-08-07 10:06:33 -0400

    fix: vercel.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
-  imports: [ UsersModule, PrismaModule,  QuizzesModule],
+  imports: [ UsersModule, QuizzesModule],
   controllers: [AppController],
   providers: [AppService],
 })
