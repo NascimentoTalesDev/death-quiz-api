38c96bf71e2b9a525f5fd4d4ef60e309307c840d
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-12-16 22:38:24 -0300

    test: test2

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,10 +16,10 @@ import { QuestionsModule } from './app/questions/questions.module';
 import { UploadsModule } from './app/uploads/uploads.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, QuizzesModule, AuthModule, UsersModule, QuestionsModule, UploadsModule],
   // imports: [ConfigModule.forRoot({ isGlobal: true }) , PrismaModule, QuizzesModule, AuthModule, UsersModule, QuestionsModule, UploadsModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, ],
   // providers: [AppService, AuthService, UsersService, UsersRepository, PrismaService, LocalStrategy, JwtService],
 })
 export class AppModule {}
